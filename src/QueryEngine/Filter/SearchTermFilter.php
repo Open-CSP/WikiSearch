2f381c6308f3a349e0c02513081a82fe30d1d25a
Marijn van Wezel <marijnvanwezel@gmail.com>
2021-04-01 16:30:02 +0200

    Automatically insert wildcards when a simple query is executed

--- src/QueryEngine/Filter/SearchTermFilter.php
+++ src/QueryEngine/Filter/SearchTermFilter.php
@@ -126,6 +126,46 @@ class SearchTermFilter implements Filter {
 		// Disable regex searches by replacing each "/" with "\/"
 		$search_term = str_replace( "/", "\\/", $search_term );
 
+		// Disable field name searches by replacing each ":" with "\:"
+
+		// Don't insert wildcard around terms when the user is performing an "advanced query"
+		$advanced_search_chars = ["\"", "'", "AND", "NOT", "OR", "~", "(", ")", "?"];
+		$is_advanced_query = array_reduce( $advanced_search_chars, function( bool $carry, $char ) use ( $search_term ) {
+			return $carry ?: strpos( $search_term, $char ) !== false;
+		}, false );
+
+		if ( !$is_advanced_query ) {
+			$search_term = $this->insertWildcards( $search_term );
+		}
+
 		return $search_term;
+	}
+
+	/**
+	 * Inserts wild cards around each term in the provided search string.
+	 *
+	 * @param string $search_string
+	 * @return string
+	 */
+	private function insertWildcards( string $search_string ): string {
+		$terms = preg_split( "/((?<=\w)\b\s*)/", $search_string, -1, PREG_SPLIT_DELIM_CAPTURE );
+
+		// $terms is now an array where every even element is a term (0 is a term, 2 is a term, etc.), and
+		// every odd element the delimiter between that term and the next term. Calling implode() on
+		// $terms gives back the original search string
+
+		$num_terms = count( $terms );
+
+		// Insert quotes around each term
+		for ( $idx = 0; $idx < $num_terms; $idx++ ) {
+			$is_term = ($idx % 2) === 0;
+
+			if ( $is_term ) {
+				$terms[$idx] = "*{$terms[$idx]}*";
+			}
+		}
+
+		// Join everything together again to get the search string
+		return implode( "", $terms );
 	}
 }
