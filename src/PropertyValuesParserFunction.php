05956908cd1d7799d0bd1f5257ff47d7e0197325
Marijn van Wezel <marijnvanwezel@gmail.com>
2022-05-20 11:39:05 +0200

    Allow base query with #prop_values parser function

--- src/PropertyValuesParserFunction.php
+++ src/PropertyValuesParserFunction.php
@@ -30,19 +30,20 @@ class PropertyValuesParserFunction {
 
 		$options = $this->extractOptions( $args );
 
-		$limit = isset( $options["limit"] ) ? $options["limit"] : "100";
-		$property = isset( $options["property"] ) ? $options["property"] : "";
-		$array_name = isset( $options["array"] ) ? $options["array"] : "";
-		$date_property = isset( $options["date property"] ) ? $options["date property"] : "Modification date";
-		$from = isset( $options["from"] ) ? $options["from"] : "1";
-		$to = isset( $options["to"] ) ? $options["to"] : "5000";
+		$limit = $options["limit"] ?? "100";
+		$property = $options["property"] ?? "";
+		$arrayName = $options["array"] ?? "";
+		$dateProperty = $options["date property"] ?? "Modification date";
+		$from = $options["from"] ?? "1";
+		$to = $options["to"] ?? "5000";
+        $baseQuery = $options["query"] ?? null;
 
-		if ( !$property || !$array_name ) {
-			return "Missing `array` or `property` parameter";
+		if ( !$property || !$arrayName ) {
+			return "Missing 'array' or 'property' parameter";
 		}
 
 		if ( !ctype_digit( $from ) || !ctype_digit( $to ) || !ctype_digit( $limit ) ) {
-			return "Invalid `from`, `limit` or `to` parameter";
+			return "Invalid 'from', 'limit' or 'to' parameter";
 		}
 
 		$from = intval( $from );
@@ -50,45 +51,38 @@ class PropertyValuesParserFunction {
 		$limit = intval( $limit );
 
 		if ( $from < 0 || $from > 9999 ) {
-			return "The `from` parameter must be a year between 0 and 10000";
+			return "The 'from' parameter must be a year between 0 and 10000";
 		}
 
 		if ( $to < 0 || $to > 9999 ) {
-			return "The `to` parameter must be a year between 0 and 10000";
+			return "The 'to' parameter must be a year between 0 and 10000";
 		}
 
 		if ( $to <= $from ) {
-			return "The `to` parameter must be greater than the `from` parameter";
+			return "The 'to' parameter must be greater than or equal to the 'from' parameter";
 		}
 
 		if ( $limit < 1 || $limit > 9999 ) {
-			return "The `limit` parameter must be an integer between 1 and 10000";
+			return "The 'limit' parameter must be an integer between 1 and 10000";
 		}
 
-		list( $from, $to ) = $this->convertDate( $from, $to );
+		list( $from, $to ) = $this->convertDates( $from, $to );
 
-		$range_filter = new PropertyRangeFilter(
-			$date_property,
-			[ "to" => $to, "from" => $from ]
-		);
+		$rangeFilter = new PropertyRangeFilter( $dateProperty, [ "to" => $to, "from" => $from ] );
+		$termsAggregation = new PropertyValueAggregation( $property, "common_values", $limit );
+		$aggregation = new FilterAggregation( $rangeFilter, [ $termsAggregation ], "property_values" );
 
-		$terms_aggregation = new PropertyValueAggregation( $property, "common_values", $limit );
+		$queryEngine = QueryEngineFactory::fromNull();
+		$queryEngine->addAggregation( $aggregation );
 
-		$aggregation = new FilterAggregation(
-			$range_filter,
-			[ $terms_aggregation ],
-			"property_values"
-		);
-
-		$query_engine = QueryEngineFactory::fromNull();
-		$query_engine->addAggregation( $aggregation );
-
-		$query = $query_engine->toArray();
+        if ( isset( $baseQuery ) ) {
+            $queryEngine->setBaseQuery( $baseQuery );
+        }
 
 		$results = ClientBuilder::create()
 			->setHosts( QueryEngineFactory::fromNull()->getElasticHosts() )
 			->build()
-			->search( $query );
+			->search( $queryEngine->toArray() );
 
 		if ( !isset( $results["aggregations"]["property_values"]["property_values"]["common_values"]["buckets"] ) ) {
 			// Failed to create aggregations
@@ -102,7 +96,7 @@ class PropertyValuesParserFunction {
 			return "";
 		}
 
-		\WSArrays::$arrays[$array_name] = new \ComplexArray( $buckets );
+		\WSArrays::$arrays[$arrayName] = new \ComplexArray( $buckets );
 
 		return "";
 	}
@@ -138,7 +132,7 @@ class PropertyValuesParserFunction {
 	 * @param int $to_year
 	 * @return array
 	 */
-	private function convertDate( int $from_year, int $to_year ): array {
+	private function convertDates( int $from_year, int $to_year ): array {
 		return [ gregoriantojd( 1, 1, $from_year ), gregoriantojd( 12, 31, $to_year ) ];
 	}
 }
