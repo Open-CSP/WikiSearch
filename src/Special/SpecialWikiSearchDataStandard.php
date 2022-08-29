650924b588891d54e84c667595cd25704eaa96d4
Marijn van Wezel <marijnvanwezel@gmail.com>
2022-08-29 14:24:17 +0200

    Use setSubmitName instead of custom button

--- src/Special/SpecialWikiSearchDataStandard.php
+++ src/Special/SpecialWikiSearchDataStandard.php
@@ -93,26 +93,19 @@ class SpecialWikiSearchDataStandard extends SpecialPage {
     private function showForm( array $descriptor ): void {
         $form = HTMLForm::factory( 'ooui', $descriptor, $this->getContext() );
 
+        $form->setSubmitName( 'wpSubmit' );
+        $form->setSubmitTextMsg( 'wikisearch-special-data-standard-submit-text' );
         $form->setSubmitCallback( [$this, 'formCallback'] );
-        $form->suppressDefaultSubmit();
         $form->showCancel();
         $form->setCancelTarget( $this->getFullTitle() );
         $form->setTokenSalt( 'wikisearchdatastandard' );
 
         $form->addButton( [
-            'name' => 'submit',
-            'value' => 'update',
+            'name' => 'wpUpdate',
+            'value' => '1',
             'class' => ['mw-htmlform-submit'],
             'label-message' => 'wikisearch-special-data-standard-submit-and-update-text',
             'flags' => [ 'primary', 'destructive' ]
-        ] );
-
-        $form->addButton( [
-            'name' => 'submit',
-            'value' => 'save',
-            'class' => ['mw-htmlform-submit'],
-            'label-message' => 'wikisearch-special-data-standard-submit-text',
-            'flags' => [ 'primary', 'progressive' ]
         ] );
 
         $form->show();
