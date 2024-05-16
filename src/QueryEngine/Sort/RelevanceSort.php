d4de211aca48d542542d24957c0598383594075a
Marijn van Wezel <marijnvanwezel@gmail.com>
2024-05-16 10:58:50 +0200

    Fix incorrect relevance sort

--- src/QueryEngine/Sort/RelevanceSort.php
+++ src/QueryEngine/Sort/RelevanceSort.php
@@ -15,46 +15,13 @@ class RelevanceSort implements Sort {
     private const SCORE_FIELD = '_score';
 
 	/**
-	 * @var string|null The order of the sort
-	 */
-	private ?string $order = null;
-
-    /**
-     * @var string|null The mode of the sort (either min, max or null)
-     */
-	private ?string $mode = null;
-
-    /**
-     * @param string|null $order
-     */
-	public function __construct( string $order = null ) {
-		switch ( $order ) {
-			case "asc":
-			case "ascending":
-			case "up":
-				$this->order = FieldSort::ASC;
-				$this->mode = "min";
-				break;
-			case "desc":
-			case "dsc":
-			case "descending":
-			case "down":
-				$this->order = FieldSort::DESC;
-				$this->mode = "max";
-				break;
-		}
-	}
-
-	/**
 	 * @inheritDoc
 	 */
 	public function toQuery(): BuilderInterface {
-        $parameters = [];
+        $parameters = [
+			'mode' => 'max'
+		];
 
-        if ( $this->mode !== null ) {
-            $parameters['mode'] = $this->mode;
-        }
-
-		return new FieldSort( self::SCORE_FIELD, $this->order, $parameters );
+		return new FieldSort( self::SCORE_FIELD, 'desc', $parameters );
 	}
 }
