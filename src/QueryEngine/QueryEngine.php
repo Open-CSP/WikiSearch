12dd933e006184dd944434d02ac743c069e3d1b9
Marijn van Wezel <marijnvanwezel@gmail.com>
2025-12-09 10:37:15 +0100

    Only add the post filters at the very end

--- src/QueryEngine/QueryEngine.php
+++ src/QueryEngine/QueryEngine.php
@@ -30,6 +30,7 @@ use WikiSearch\WikiSearchServices;
 class QueryEngine {
     private const DEFAULT_RESULT_LIMIT = 10;
     private const AGGREGATION_PROPERTY_PARAMETER = '_wikisearch_aggregation_property';
+    private const FILTER_FIELD_PARAMETER = '_wikisearch_filter_field';
 
 	/**
 	 * @var Search
@@ -159,39 +160,13 @@ class QueryEngine {
      */
     public function addPostFilter( AbstractFilter $filter, string $occur = BoolQuery::MUST ): void {
         $filterQuery = $filter->toQuery();
+
+        if ( $filter instanceof PropertyFilter ) {
+            // Store which field this filter affects
+            $filter->{self::FILTER_PROPERTY_PARAMETER} = $filter->getField();
+        }
+
         $this->search->addPostFilter( $filterQuery, $occur );
-
-        // Post filters are applied after aggregations have been calculated. This makes it possible to have facets
-        // that do not change after the filter they control is applied. This is useful for facets that should act like
-        // a disjunction, where adding a filter should increase the number of results. However, this leaves us with the
-        // problem that other aggregations that should be affected by the added filter are no longer accurate. To solve
-        // this, we also add any post filter to the all other aggregations using a FilterAggregation.
-
-        if ( !$filter instanceof PropertyFilter ) {
-            return;
-        }
-
-        foreach ( $this->search->getAggregations() as $aggregation ) {
-            if ( !$aggregation instanceof FilterAggregation ) {
-                continue;
-            }
-
-            if (
-                isset( $aggregation->{self::AGGREGATION_PROPERTY_PARAMETER} ) &&
-                $aggregation->{self::AGGREGATION_PROPERTY_PARAMETER}->getPID() === $filter->getField()->getPID()
-            ) {
-                // This aggregation affects the same property
-                continue;
-            }
-
-            $aggregationFilter = $aggregation->getFilter();
-
-            if ( !$aggregationFilter instanceof BoolQuery ) {
-                continue;
-            }
-
-            $aggregationFilter->add( $filterQuery );
-        }
     }
 
 	/**
@@ -292,6 +267,8 @@ class QueryEngine {
         $this->search->setSource( $this->sources );
 
 		$newSearch = clone $this->search;
+
+        $this->addPostFiltersToAggregations( $newSearch );
 		$this->addSortsToSearch( $newSearch );
 
         $query = [
@@ -328,4 +305,42 @@ class QueryEngine {
 			$search->addSort( $fallbackSort->toQuery() );
 		}
 	}
+
+    /**
+     * Add the post filters to the aggregations in the given Search object.
+     *
+     * Post filters are applied after aggregations have been calculated. This makes it possible to have facets
+     * that do not change after the filter they control is applied. This is useful for facets that should act like
+     * a disjunction, where adding a filter should increase the number of results. However, this leaves us with the
+     * problem that other aggregations that should be affected by the added filter are no longer accurate. To solve
+     * this, we also add any post filter to the all other aggregations using a FilterAggregation.
+     *
+     * @param Search $search
+     * @return void
+     */
+    private function addPostFiltersToAggregations( Search $search ): void {
+        foreach ( $search->getPostFilters() as $filter ) {
+            foreach ( $search->getAggregations() as $aggregation ) {
+                if ( !$aggregation instanceof FilterAggregation ) {
+                    continue;
+                }
+
+                if (
+                    isset( $aggregation->{self::AGGREGATION_PROPERTY_PARAMETER}) &&
+                    $aggregation->{self::AGGREGATION_PROPERTY_PARAMETER}->getPID() === $filter->{self::FILTER_FIELD_PARAMETER}->getPID()
+                ) {
+                    // This aggregation affects the same property
+                    continue;
+                }
+
+                $aggregationFilter = $aggregation->getFilter();
+
+                if (!$aggregationFilter instanceof BoolQuery) {
+                    continue;
+                }
+
+                $aggregationFilter->add( $filter );
+            }
+        }
+    }
 }
